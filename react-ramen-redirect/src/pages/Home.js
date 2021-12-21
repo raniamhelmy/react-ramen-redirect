56eaac6f40094934f9b4f556119d880e8e272f43
Rania-Helmy <ranmhelmy@gmail.com>
2021-12-21 19:06:42 +0300

    remove pages comments

--- react-ramen-redirect/src/pages/Home.js
+++ react-ramen-redirect/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../App.css";
-//import HeroSection from "../HeroSection";
 import Products from "../components/Products";
 import Desserts from "../components/Desserts";
 import Speciality from "../components/Speciality";
@@ -11,12 +10,10 @@ import { ramenData, dessertData, drinksData } from "../data";
 function Home() {
   return (
     <>
-      {/*<HeroSection />*/}
       <Products heading={"Choose your Favorite"} data={ramenData} />
       <Speciality />
       <Desserts heading={"Sweet Treats for You"} data={dessertData} />
       <Drinks heading={"Beverages from Heaven"} data={drinksData} />
-      {/*<More/>*/}
       <Footer />
     </>
   );
